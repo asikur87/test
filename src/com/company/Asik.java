1a1fa68bc01d9501513fcac72586be55396efc5f
asikur87 <learnerasik.25@gmail.com>
2021-09-25 23:15:14 +0600

    tyhg

---
+++ src/com/company/Asik.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Asik{
+    //x=5;
+
+   // System.out.println("Bismillahir Rahmanir Rahim");
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.sun.source.util.SourcePositions;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=5;
+        //System.out.println("Bismillahir Rahmanir Rahim");
+        System.out.println(a);
+        Scanner sc=new Scanner(System.in);
+    }
+}
